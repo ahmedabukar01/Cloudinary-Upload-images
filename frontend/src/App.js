b9fb89ffd4c46413899bcfe01613da9078d6b5de
ahmed <ahmedlearn01@gmail.com>
2022-08-28 09:45:21 +0300

    image displayed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [file,setFile] = useState('')
+  const [image, setImage] = useState('')
+
+  const onFileChange = (e)=>{
+    const f = e.target.files[0];
+    preveiwImage(f);
+  }
+  const preveiwImage = (f)=>{
+    const reader = new FileReader();
+    reader.readAsDataURL(f);
+    reader.onloadend = () =>{
+      setImage(reader.result)
+    }
+  }
   return (
     <div className="App">
       <h1>Hello</h1>
+      <form>
+        <input type='file' name="image" value={file} onChange={onFileChange}/>
+      </form>
+
+      <h2>Image</h2>
+      {image && (
+        <img src={image} alt="myimage" style={{width: '300px'}}/>
+      )}
     </div>
   );
 }
