e52589694ac4964f06571fb8dd0a0024a2fabbbd
ahmed <ahmedlearn01@gmail.com>
2022-08-28 17:10:37 +0300

    preset

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,16 @@ app.use(cors())
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}))
 
+// get images
+app.get('/api/images', async (req,res)=>{
+    const {resources} = await cloudinary.search.expression('mystore')
+    .sort_by('public_id', 'desc')
+    .execute();
+    const publicIds = resources.map(res=> res.public_id);
+    res.json(publicIds);
+    console.log(publicIds)
+})
+
 app.post('/api/upload', async (req,res)=>{
    try {
     const fileStr = req.body.img
